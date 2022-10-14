7b57a40cfed70b1eef7b7837618271a02760c919
DavidNojnarg <dgranjon@ymail.com>
2022-10-14 20:13:59 +0000

    added p

--- R/typography.R
+++ R/typography.R
@@ -36,3 +36,55 @@ dracula_heading <- function(..., tagName = "h1", color = "purple",
     ...
   )
 }
+
+#' Dracula paragraph
+#'
+#' @param ... Text.
+#' @param color Text color.
+#' @param align Text alignement.
+#' @param weight Text weight.
+#' @param size Text size.
+#' @param lineHeight Text line height.
+#'
+#' @seealso \url{https://ui.draculatheme.com/paragraph}.
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'  library(shiny)
+#'  library(dRacula)
+#'  ui <- dracula_page(
+#'   dracula_p(
+#'     "Dracula is an 1897 Gothic horror novel by Irish author Bram Stoker. It
+#'      introduced the character of Count Dracula and established many conventions
+#'      of subsequent vampire fantasy. The novel tells the story of Dracula's
+#'      attempt to move from Transylvania to England so that he may find new blood
+#'      and spread the undead curse, and of the battle between Dracula and Professor
+#'      Abraham Van Helsing.",
+#'     color = "pink-purple",
+#'     align = "center",
+#'     weight = "bold",
+#'     size = "lg",
+#'     lineHeight = "xs"
+#'   )
+#'  )
+#'  server <- function(input, output, session) {
+#'  }
+#'  shinyApp(ui, server)
+#' }
+dracula_p <- function(..., color = "white", align = "left",
+                      weight = "normal", size = "md",
+                      lineHeight = "md") {
+  tags$p(
+    class = sprintf(
+      "drac-text drac-text-%s drac-line-height drac-text-%s
+      drac-text-%s drac-text-%s drac-line-height-%s",
+      align,
+      color,
+      weight,
+      size,
+      lineHeight
+    ),
+    ...
+  )
+}
--- inst/examples/base/app.R
+++ inst/examples/base/app.R
@@ -4,7 +4,18 @@ library(dRacula)
 ui <- dracula_page(
   dracula_heading("Hello World", color = "cyan-green"),
   dracula_card(
-    "My first card"
+    dracula_p(
+      "Dracula is an 1897 Gothic horror novel by Irish author Bram Stoker. It
+     introduced the character of Count Dracula and established many conventions
+     of subsequent vampire fantasy. The novel tells the story of Dracula's
+     attempt to move from Transylvania to England so that he may find new blood
+     and spread the undead curse, and of the battle between Dracula and Professor
+     Abraham Van Helsing.",
+     color = "black-secondary",
+     weight = "bold",
+     size = "lg",
+     lineHeight = "xs"
+    )
   ),
   dracula_card(
     "My second card",
